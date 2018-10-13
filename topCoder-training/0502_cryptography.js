03b4b5e1e42968cf3c39f82387117a8265ac0825
awer000 <awer000@naver.com>
2018-10-14 08:22:22 +0900

    5장 암호화 문제 다른 방법 이용해서 풀이 완료

--- topCoder-training/0502_cryptography.js
+++ topCoder-training/0502_cryptography.js
@@ -12,7 +12,15 @@ function solution(arr) {
   return answer;
 }
 
-console.log(solution([1, 2, 3]));
-console.log(solution([1, 3, 2, 1, 1, 3]));
-console.log(solution([1000, 999, 998, 997, 996, 995]));
-console.log(solution([1, 1, 1, 1]));
+// 가장 최소값에 1을 더하면 모두 곱했을때 가장 큰 수가 나온다는 사실을 이용한 코드
+
+function solution2(arr) {
+  arr = arr.sort((a, b) => a - b);
+  arr[0]++;
+  return arr.reduce((a, b) => a * b);
+}
+
+console.log(solution2([1, 2, 3]));
+console.log(solution2([1, 3, 2, 1, 1, 3]));
+console.log(solution2([1000, 999, 998, 997, 996, 995]));
+console.log(solution2([1, 1, 1, 1]));
