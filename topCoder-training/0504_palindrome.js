cfcf03b57c00882ddb910c6675e362747f752730
awer000 <awer000@naver.com>
2018-10-15 18:27:47 +0900

    탑코드 0504문제 다른 접근 풀이중

--- topCoder-training/0504_palindrome.js
+++ topCoder-training/0504_palindrome.js
@@ -33,3 +33,30 @@ console.log(solution("abab"));
 console.log(solution("abacaba"));
 console.log(solution("qwerty"));
 console.log(solution("a;lskdjfkldsjflkdjslkfjslkfjwww"));
+
+// 아래 코드는 다른 접근
+
+//function solution2(s) {
+//   if (
+//     s ===
+//     s
+//       .split("")
+//       .reverse()
+//       .join("")
+//   )
+//     return s.length;
+//   let min = 0;
+//   let max = s.length - 1;
+//   let answer = s.length;
+//   while (min < max) {
+//     if (s[min] !== s[max]) {
+//       answer++;
+//       min++;
+//       max--;
+//     } else {
+//       min++;
+//       max--;
+//     }
+//   }
+//   return answer;
+// }
