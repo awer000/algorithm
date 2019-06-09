3af74f58a51246aa3e297fa80da60953b3fac671
awer000 <awer000@naver.com>
2019-06-09 15:52:42 +0900

    09 - 리팩토링

--- topCoder-training/20190609_NumberMagicEasy.js
+++ topCoder-training/20190609_NumberMagicEasy.js
@@ -1,13 +1,13 @@
 function solution(answer) {
-  const binary_answer = [] ;
+  let binary_answer = "" ;
   answer.split("").map(item => {
     if (item === "Y") {
-      binary_answer.push(0)
+      binary_answer = binary_answer + '0'
     } else {
-      binary_answer.push(1)
+      binary_answer = binary_answer + '1'
     }
   })
-  return parseInt(Number(binary_answer.join("")), 2) + 1; 
+  return parseInt(Number(binary_answer), 2) + 1; 
 }
 
 console.log(solution("YNNY"))
